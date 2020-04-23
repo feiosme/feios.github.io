57f3f1d0f5a12af18cf9bc322d79e78ea0825d1d
feiosme <1327979498@qq.com>
2020-04-24 02:47:24 +0800

    Site updated: 2020-04-24 02:47:23

---
+++ js/fancybox.js
@@ -0,0 +1,19 @@
+if($("img[alt^='?']")){
+
+$("#nexmoe-content img:not(.LinkCard-image)").each(function() {
+  $(this).attr("data-src", $(this).attr("src"));
+  $(this).attr("src", "");
+  $(this).addClass("lazyload");
+  $(this).attr("referrerPolicy", "no-referrer");
+});
+
+$("article:not(.nexmoe-py) img:not(.LinkCard-image)").each(function() {
+  if($(this).parent()[0].localName!=="a"){
+    var element = document.createElement("a");
+    $(element).attr("data-fancybox", "gallery");
+    $(element).attr("href", $(this).attr("data-src"));
+    $(this).wrap(element);
+  } 
+});
+
+}
